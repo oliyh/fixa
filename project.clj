dbb6af8ece4c783c92ef4bb712ee2ee34c3ada10
Oliver Hine <git@oliy.co.uk>
2022-08-11 14:21:38 +0100

    Version 0.1.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.github.oliyh/fixa "0.1.0"
+(defproject com.github.oliyh/fixa "0.1.1-SNAPSHOT"
   :description "Better fixtures for Clojure tests"
   :url "https://github.com/oliyh/fixa"
   :license {:name "The MIT License"
